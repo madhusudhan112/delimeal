13a33c32d4e3937aebb34065ef227964603d70a5
madhusudhan112 <madhusudhan.ghimire@asparksys.com>
2021-12-07 13:39:11 +0545

    fix: Drawer

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,8 +1,49 @@
 import 'package:flutter/material.dart';
 
 class MainDrawer extends StatelessWidget {
+  Widget buildListTile(String title, IconData icon) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 26,
+      ),
+      title: Text(
+        title,
+        style: const TextStyle(
+            fontFamily: 'RobotoCondensed',
+            fontSize: 20,
+            fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(20.0),
+            height: 120,
+            width: double.infinity,
+            alignment: Alignment.centerLeft,
+            color: Theme.of(context).accentColor,
+            child: Text(
+              "Cooking App",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w900,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          buildListTile("Meal", Icons.restaurant),
+          buildListTile("Filters", Icons.settings),
+        ],
+      ),
+    );
   }
 }
